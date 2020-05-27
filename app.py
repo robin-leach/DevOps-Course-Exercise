993e5a6a2ff8cd4b5bb4764da2167daf5ddc6ec3
Robin Leach <robin.leach@softwire.com>
2020-05-27 15:16:06 +0100

    Complete core exercise

--- app.py
+++ app.py
@@ -4,9 +4,13 @@ import session_items as session
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return 'Hello World!'
+    if request.method == "POST":
+        session.add_item(request.form.get('title'))
+
+    items = session.get_items()
+    return render_template('index.html', items=items)
 
 if __name__ == '__main__':
     app.run()
